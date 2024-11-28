6655e56549baa296467de41ecc7c15b67da3917c
Karen Otake <whitechocolate72a@gmail.com>
2024-11-28 12:10:29 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Karen Otake
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
 	echo ${1}行目が違うよ
 	res=1
@@ -8,8 +9,18 @@ ng () {
 
 res=0
 
+### NOMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
-[ "${res}" = 0 ] && echo OK
 
+### STRANGE INPUT ###
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
