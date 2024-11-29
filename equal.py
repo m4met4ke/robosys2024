90fcaff99e5916e0215e4cebc0ba648a14b4e53b
Karen Otake <whitechocolate72a@gmail.com>
2024-11-30 03:31:51 +0900

    Exception handling

--- equal.py
+++ equal.py
@@ -7,22 +7,30 @@ import itertools
 keyboard_input = input()
 numbers = list(map(int, keyboard_input.split()))
 
-permutations = list(itertools.permutations(numbers))
+def find_equations(numbers):
+    permutations = list(itertools.permutations(numbers))
 
-operators = ['+', '-', '*', '/']
-operator_combinations = list(itertools.permutations(operators))
+    operators = ['+', '-', '*', '/']
+    operator_combinations = list(itertools.permutations(operators))
 
-results = []
+    results = []
 
-for perm in permutations:
-    for ops in operator_combinations:
-        expression = f"{perm[0]} {ops[0]} {perm[1]} {ops[1]} {perm[2]} {ops[2]} {perm[3]}"
-        try:
-            if eval(expression) == 0:
-                results.append(expression + " = 0")
-        except ZeroDivisonError:
-            continue
+    for perm in permutations:
+        for ops in operator_combinations:
+            expression = f"{perm[0]} {ops[0]} {perm[1]} {ops[1]} {perm[2]} {ops[2]} {perm[3]}"
+            try:
+                if eval(expression) == 0:
+                    results.append(expression + " = 0")
+            except ZeroDivisonError:
+                continue
+    
+    return results
 
-for eq in results:
-    print(eq)
+if len(numbers) != 4:
+    print("四つの数字を入力")
+else:
+    equations = find_equations(numbers) 
+
+    for eq in equations:
+        print(eq)
 
