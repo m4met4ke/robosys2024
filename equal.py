012eaabc10ec17f153acc993bd81209a9b36d4c3
Karen Otake <whitechocolate72a@gmail.com>
2024-11-30 02:55:45 +0900

    Make the equal sign work

--- equal.py
+++ equal.py
@@ -6,21 +6,22 @@ import itertools
 
 keyboard_input = input()
 numbers = list(map(int, keyboard_input.split()))
-print(numbers)
 
 permutations = list(itertools.permutations(numbers))
-print(permutations)
 
 operators = ['+', '-', '*', '/']
 operator_combinations = list(itertools.permutations(operators))
-print(operator_combinations)
 
 results = []
 
 for perm in permutations:
     for ops in operator_combinations:
         expression = f"{perm[0]} {ops[0]} {perm[1]} {ops[1]} {perm[2]} {ops[2]} {perm[3]}"
-        results.append(expression)
+        try:
+            if eval(expression) == 0:
+                results.append(expression + " = 0")
+        except ZeroDivisonError:
+            continue
 
 for eq in results:
     print(eq)
